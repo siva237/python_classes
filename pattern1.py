0cc92d315ea3c31a6c8ee727de86dcc0dc1fe521
siva237 <sivasankar.putturu@gmail.com>
2019-05-27 15:29:43 +0530

    python practice programs

--- pattern1.py
+++ pattern1.py
@@ -1,10 +1,14 @@
-def pattern1():
-    num=1
-    for i in range(1,5):
-        for j in range(1,i+1):
-            print( i*j ,end=' ')
+# def pattern1():
+#     num=1
+#     for i in range(1,5):
+#         for j in range(1,i+1):
+#             print( i*j ,end=' ')
+#         print()
+# pattern1()
 
-        print()
-pattern1()
 
-
+n=int(input('enter the vlaue:'))            #   3
+for num in range(n+1):                      # o/p: 1
+    for i in range(num):                     #     22
+        print(num,end='')                    #     333
+    print("\n")
